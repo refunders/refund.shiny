35652f3992ba227ece8675132bcc93e81cf6711d
Julia Wrobel <jw3134@cumc.columbia.edu>
2017-07-07 13:52:52 -0400

    bug fixes: updated dependencies

--- R/plot_shiny.registration.R
+++ R/plot_shiny.registration.R
@@ -14,7 +14,7 @@
 #'
 #' @seealso \code{\link{plot_shiny}}
 #' @importFrom gridExtra grid.arrange
-#' @importFrom plotly plot_ly ggplotly event_data layout
+#' @importFrom plotly plot_ly event_data layout add_trace
 #'
 #' @export
 #'
@@ -22,15 +22,17 @@ plot_shiny.registration = function(obj, xlab = "", ylab="", title = "", ...){
   reg.obj <- obj$reg_obj
   fpca.obj <- obj$fpca_obj
 
+  ## establish inverse link function for plotting
+  inv_link = createInvLink(family = fpca.obj$family)
+
   ## data management. Probably should think about generalizing this to other distributions.
   Y <- reg.obj$Y
   Y$t.star <- obj$time_warps[[1]]
   Y$t.hat <- Y$index
   Y$Y.hat <- fpca.obj$Yhat$value
-  Y$pi.hat <- inv.logit(Y$Y.hat)
+  #Y$pi.hat <- inv.logit(Y$Y.hat)
+  Y$pi.hat <- inv_link(Y$Y.hat)
 
-  ## establish inverse link function for plotting
-  inv_link = createInvLink(family = fpca.obj$family)
 
   ## add y-axis scale input if family is not gaussian
   #if (!(is.null(fpca.obj$family) || fpca.obj$family == "gaussian")) {
