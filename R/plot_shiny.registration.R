b005ce4fc545dba79c173546198f941beca126dc
Julia Wrobel <jw3134@cumc.columbia.edu>
2017-07-27 20:29:04 -0400

    added mu +/- PC plot for registration page

--- R/plot_shiny.registration.R
+++ R/plot_shiny.registration.R
@@ -28,15 +28,11 @@ plot_shiny.registration = function(obj, xlab = "", ylab="", title = "", ...){
   Y$t.hat <- Y$index
   Y$Y.hat <- fpca.obj$Yhat$value
   Y$pi.hat <- inv.logit(Y$Y.hat)
+  Yhat_df <- fpca.obj$Yhat
+  Y_df <- fpca.obj$Y
 
   ## establish inverse link function for plotting
   inv_link = createInvLink(family = fpca.obj$family)
-
-  ## add y-axis scale input if family is not gaussian
-  #if (!(is.null(fpca.obj$family) || fpca.obj$family == "gaussian")) {
-    #muPC.call[[2]] = selectInput("muPC_scale", label = ("Select Y-axis Scale"), choices = c("Natural", "Response"), selected = "Natural")
-    #LinCom.call[[fpca.obj$npc + 1]] = selectInput("lincom_scale", label = ("Select Y-axis Scale"), choices = c("Natural", "Response"), selected = "Natural")
-  #}
 
   ################################
   ## code for processing tabs
@@ -62,6 +58,11 @@ plot_shiny.registration = function(obj, xlab = "", ylab="", title = "", ...){
                indicate the population mean +/- the FPC times 2 SDs of the associated score distribution."
   muPC.call = as.list(NA)
   muPC.call[[1]] = selectInput(inputId = "PCchoice", label = ("Select FPC"), choices = 1:fpca.obj$npc, selected = 1)
+
+  ## add y-axis scale input if family is not gaussian
+  if (!(is.null(fpca.obj$family) || fpca.obj$family == "gaussian")) {
+    muPC.call[[2]] = selectInput("muPC_scale", label = ("Select Y-axis Scale"), choices = c("Natural", "Response"), selected = "Natural")
+  }
 
   # subject fits plot
 
@@ -161,9 +162,66 @@ plot_shiny.registration = function(obj, xlab = "", ylab="", title = "", ...){
 
       callModule(tabPanelModule, "loss", plotObject = plotInputLoss, plotName = "loss")
 
+
+      #################################
+      ## Define fpca objects
+      #################################
+
+      ## define global objects
+      mu_df = as_refundObj(matrix(fpca.obj$mu, nrow = 1), index = fpca.obj$argvals)
+      efunctions = matrix(fpca.obj$efunctions, ncol = fpca.obj$npc)
+      sqrt.evalues = diag(sqrt(fpca.obj$evalues), fpca.obj$npc, fpca.obj$npc)
+      scaled_efunctions = efunctions %*% sqrt.evalues
+
+      ## prep objects for plotting on response scale; used in subject plot tabs
+      mu_df_inv_link = mu_df
+      mu_df_inv_link = mutate(mu_df_inv_link, value = inv_link(value))
+
+      Yhat_df_inv_link = Yhat_df
+      Yhat_df_inv_link = mutate(Yhat_df_inv_link, value = inv_link(value))
+
+      ## define plot defaults
+      ## set y axes to be max(2 SDs from mu of PC1, fitted values)
+      max.y = max(fpca.obj$mu + 2 * abs(scaled_efunctions[, 1]))
+      min.y = min(fpca.obj$mu - 2 * abs(scaled_efunctions[, 1]))
+
+      plotDefaults = list(theme = theme_bw(),
+                          title = theme(plot.title = element_text(size = 20)),
+                          xlab = xlab(xlab),
+                          ylab = ylab(ylab),
+                          ylim = ylim(c(min(min.y, range(Yhat_df$value)[1]), max(max.y,range(Yhat_df$value)[2]))),
+                          x_scale = scale_x_continuous(breaks = seq(0, length(fpca.obj$mu) - 1, length = 6),
+                                                       labels = paste0(c(0, 0.2, 0.4, 0.6, 0.8, 1))))
+
       #################################
       ## Code for plot of fpca mean +/- PCs
       #################################
+
+      plotInputMuPC <- reactive({
+        PCchoice = as.numeric(input$PCchoice)
+        scaled_efuncs = scaled_efunctions[,PCchoice]
+
+        df_plus = as_refundObj(matrix(fpca.obj$mu + 2 * scaled_efuncs, nrow = 1), index = fpca.obj$argvals)
+        df_plus$id = 2
+
+        df_minus = as_refundObj(matrix(fpca.obj$mu - 2 * scaled_efuncs, nrow = 1), index = fpca.obj$argvals)
+        df_minus$id = 3
+
+        plot_df = bind_rows(mu_df, df_plus, df_minus) %>%
+          mutate(id = as.character(id))
+
+        if (!(is.null(fpca.obj$family) || fpca.obj$family == "gaussian") && input[["muPC_scale"]] == "Response") {
+          plot_df = mutate(plot_df, value = inv_link(value))
+          plotDefaults[["ylim"]] = ylim(c(range(Y_df$value)[1], range(Y_df$value)[2]))
+        }
+
+        p1 <- ggplot(filter(plot_df, id == "1"), aes(x = index, y = value)) + geom_line(lwd = 1) + plotDefaults +
+          geom_point(data = filter(plot_df, id == "2"), color = "blue", size = 4, shape = '+') +
+          geom_point(data = filter(plot_df, id == "3"), color = "indianred", size = 4, shape = "-") +
+          ggtitle(bquote(psi[.(input$PCchoice)]~(t) ~ "," ~.(100*round(fpca.obj$evalues[as.numeric(input$PCchoice)]/sum(fpca.obj$evalues),3)) ~ "% Variance"))
+      })
+
+      callModule(tabPanelModule, "muPC", plotObject = plotInputMuPC, plotName = "muPC")
 
 
     } # end server
